875cf8e686c7a9f642276ecdc4e7ef74e690cfdd
Mr. Rafiqul Islam <rafiqulislamemon05@gmail.com>
2025-06-26 02:46:59 +0600

    routing setup done

--- app/(website)/(pages)/(dashboard)/page.tsx
+++ app/(website)/(pages)/(dashboard)/page.tsx
@@ -1,11 +1,9 @@
-import Header from "@/components/Header";
 import HeroSection from "@/components/HeroSection";
 import TaskBoard from "@/components/task/TaskBoard";
 
 export default function Home() {
   return (
     <>
-      <Header/>
       <HeroSection/>
       <TaskBoard/>
     </>
---
+++ app/(website)/(pages)/tasks/[id]/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function TaskDetails() {
+  return (
+    <div className='container'>
+        
+        <h1 className='text-3xl text-white'>task 1</h1>
+    </div>
+  )
+}
