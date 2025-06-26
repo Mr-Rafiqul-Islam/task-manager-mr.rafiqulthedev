5de9524d8417271fc3a8b7270863a88cc4ea4099
Mr. Rafiqul Islam <rafiqulislamemon05@gmail.com>
2025-06-27 02:09:28 +0600

    Feat: Add Task Modal added

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,20 @@
 import Image from "next/image";
 import Link from "next/link";
-import logo from "../public/logo.webp"
+import logo from "../public/logo.webp";
 
 export default function Header() {
-    return (
-        <nav className="py-6 md:py-8 sticky top-0 w-full !bg-[#191D26] z-50">
-            <div className="container mx-auto flex items-center justify-between gap-x-6">
-                <Link href="/">
-                    <Image
-                        className="h-[45px]"
-                        src={logo}
-                        alt="Lws"
-                    />
-                </Link>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="py-6 md:py-8 sticky top-0 w-full !bg-[#191D26] z-50">
+      <div className="container mx-auto flex items-center justify-between gap-x-6">
+        <Link href="/">
+          <Image
+            className="h-[45px]"
+            src={logo}
+            alt="Lws"
+            style={{ width: "auto", height: "auto" }}
+          />
+        </Link>
+      </div>
+    </nav>
+  );
 }
--- components/task/TaskBoard.tsx
+++ components/task/TaskBoard.tsx
@@ -1,15 +1,50 @@
-import React from "react";
+'use client'
+import React, { useState } from "react";
 import FilterBox from "./FilterBox";
 import TaskActions from "./TaskActions";
 import TaskList from "./TaskList";
+import AddTaskModal, { Task } from "./AddTaskModal";
 
 export default function TaskBoard() {
+
+
+    const [showAddModal, setShowAddModal] = useState(false);
+    const [taskToUpdate, setTaskToUpdate] = useState(null);
+
+    function handleAddEditTask(newTask:Task, isAdd:boolean) {
+        if (isAdd) {
+            // setTasks([...tasks, newTask]);
+        } else {
+            // setTasks(
+            //     tasks.map((task) => {
+            //         if (task.id === newTask.id) {
+            //             return newTask;
+            //         }
+            //         return task;
+            //     })
+            // );
+        }
+
+       handleCloseClick();
+    }
+
+    function handleCloseClick() {
+        setShowAddModal(false);
+        setTaskToUpdate(null);
+    }
   return (
     <section className="mb-20" id="tasks">
       <div className="container">
         <FilterBox/>
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-12">
-          <TaskActions/>
+        {showAddModal && (
+                <AddTaskModal
+                    onSave={handleAddEditTask}
+                    onCloseClick={handleCloseClick}
+                    taskToUpdate={taskToUpdate}
+                />
+            )}
+          <TaskActions onAddClick={()=>setShowAddModal(true)} onDeleteAllClick={()=>{}}/>
           <TaskList/>
         </div>
       </div>
